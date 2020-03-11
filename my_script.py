1db94f8104928010234636cee7fd774cfa51bc9d
Julio E <je27@protonmail.com>
2020-03-10 21:05:29 -0700

    Dev packages

--- my_script.py
+++ my_script.py
@@ -2,3 +2,10 @@ import pandas
 
 print("HAPPY TUESDAY NIGHT")
 
+from julioe27-lambdadata.mymod import enlarge
+
+df = pandas.DataFrame({x:})
+print(df.head())
+
+x = 5
+print("ENLARGE",x,"TO", enlarge(x))
