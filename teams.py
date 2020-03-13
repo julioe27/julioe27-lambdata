551538c00f54de2bd45caee69523d079a234d656
Julio E <je27@protonmail.com>
2020-03-12 21:09:37 -0700

    Overriding function for pitcher

--- teams.py
+++ teams.py
@@ -1,63 +1,57 @@
-# example teams.py (functional approach)
-# def advertise(my_team):
-#     print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
-# def full_name(my_team):
-#     return f"{my_team['city']} {my_team['name']}"
-# if __name__ == "__main__":
-#     teams = [
-#         {"city": "New York", "name": "Yankees"},
-#         {"city": "New York", "name": "Mets"},
-#         {"city": "Boston", "name": "Red Sox"},
-#         {"city": "New Haven", "name": "Ravens"},
-#         {"city": "Washington", "name": "Nationals"}
-#     ]
-#     for team in teams:
-#         print("-------")
-#         print(full_name(team))
-#         advertise(team)
-
+# example teams.py (OOP inheritance approach)
 class Team():
-    # init = initilizater
-    def __init__(self, name, city):
+    def __init__(self, name, city="CITY NAME", players=["Player 1"]):
         self.name = name
         self.city = city
-
-    # method
+        self.players = players
     def advertise(self):
-        print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+        #print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
         print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
-
-    # property
     @property
-    # @property, what is this exactly?
     def full_name(self):
+        #return f"{my_team['city']} {my_team['name']}"
         return f"{self.city} {self.name}"
-
-
+class BaseballTeam(Team):
+    def __init__(self, name, city, starting_pitcher, players=["Player 1"]):
+        super().__init__(name, city, players)
+        self.starting_pitcher = starting_pitcher
+    def advertise(self):
+        #print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR PITCHER {self.starting_pitcher}!!!")
 if __name__ == "__main__":
+    football_team = Team("Cowboys", "Dallas")
+    print(football_team.full_name)
+    football_team.advertise()
     teams = [
-        {"city": "New York", "name": "Yankees"},
-        {"city": "New York", "name": "Mets"},
-        {"city": "Boston", "name": "Red Sox"},
-        {"city": "New Haven", "name": "Ravens"},
-        {"city": "Washington", "name": "Nationals"}
+        {"city": "New York", "name": "Yankees", "pitcher": "John"},
+        {"city": "New York", "name": "Mets", "pitcher": "Jane"},
+        {"city": "Boston", "name": "Red Sox",  "pitcher": "Ames"},
+        {"city": "New Haven", "name": "Ravens",  "pitcher": "Vishal"},
+        {"city": "Washington", "name": "Nationals",  "pitcher": "Cody"}
     ]
     for team_attributes in teams:
-        team = Team(name=team_attributes["name"], city=team_attributes["city"])
+        team = BaseballTeam(name=team_attributes["name"], city=team_attributes["city"], starting_pitcher=team_attributes["pitcher"])
         print("-------")
-        # print(full_name(team))
-        # advertise(team)
+        #print(full_name(team))
+        #advertise(team)
         print(team.city)
-        print(team.full_name())
+        print(team.full_name)
+        print(team.players)
+        print(team.starting_pitcher)
         team.advertise()
-
-    # team = Team(city="Washington", name="Wizards") # initialize/ create an instance of the object
-    # print(team)
-    # print(type(team))
-    # print(team.name) # invoking attributes
-    # print(team.city) # invoking attributes
-    # team2 = Team("Giants", "New York") # initialize/ create an instance of the object
-    # print(team2)
-    # print(type(team2))
-    # print(team2.name) # invoking attributes
-    # print(team2.city) # invoking attributes
+    # df1 = DataFrame({"x": [1,2,3], "y": [4,5,6]})
+    # df1.head()
+    # df1.columns
+    # df2 = DataFrame({"x": [7,7,7], "y": [4,4,4]})
+    # df2.head()
+    #team = Team(city="Washington", name="Wizards") # initialize / create an instance of the object
+    #print(team)
+    #print(type(team))
+    #print(team.name) # invoking attributes
+    #print(team.city) # invoking attributes
+#
+    #team2 = Team("Giants", "New York") # initialize / create an instance of the object
+    #print(team2)
+    #print(type(team2))
+    #print(team2.name) # invoking attributes
+    #print(team2.city) # invoking attributes
